0403cd7be4d912d7c3f4d12dac7378e9269e3cdf
Kleytonmr <kleytonmatosramos@gmail.com>
2024-02-03 13:06:05 -0300

    Initial Commit

---
+++ Dinheirama/DinheiramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinheiramaApp.swift
+//  Dinheirama
+//
+//  Created by Kleyton Matos Ramos on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DinheiramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
