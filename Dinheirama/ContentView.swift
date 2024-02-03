d5ffc4fc2e37c7b702f8133c0a9771721415e951
Kleytonmr <kleytonmatosramos@gmail.com>
2024-02-03 17:19:39 -0300

    fixed list and add function OnDelete to remove items

--- Dinheirama/ContentView.swift
+++ Dinheirama/ContentView.swift
@@ -27,19 +27,20 @@ struct ItemView: View {
 
 struct ButtonlView: View {
     var body: some View {
-        HStack {
+        VStack {
             Button("Adicionar Item") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }  
-            .fontWeight(.bold)
-            .frame(width: 150)
-            
-            Spacer()
-            Text("R$ 100,00")
-                .fontWeight(.bold)
-                .frame(width: 150)
+                // Código para adicionar um novo item
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color(red: 0.818, green: 0.652, blue: 0.218))
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 1) // Adiciona uma borda curva preta de 1 ponto
+            )
         }
-        .padding()
+        .frame(width: 200)
     }
 }
 
@@ -48,11 +49,11 @@ struct LabelView: View {
         HStack {
             Text("Nome")
                 .fontWeight(.bold)
-                .frame(width: 150)
+                .frame(width: 120)
             Spacer()
             Text("Valor")
                 .fontWeight(.bold)
-                .frame(width: 150)
+                .frame(width: 120)
         }
         .padding()
     }
@@ -61,43 +62,59 @@ struct LabelView: View {
 struct ContentView: View {
     @State private var items = [
         Item(name: "Name of Item 1", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
-        Item(name: "Name of Item 2", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
+        Item(name: "Name of Item 1", value: 10.0),
     ]
 
     var body: some View {
-        VStack {
-            LabelView()
-                .padding(.horizontal)
-            ScrollView {
-                VStack(alignment: .leading) {
+        NavigationView {
+            List {
+                Section(header: LabelView()) {
                     ForEach(items) { item in
                         ItemView(item: item)
                             .listRowInsets(EdgeInsets())
                             .cornerRadius(10)
                             .padding(.horizontal)
                     }
+                    .onDelete(perform: deleteItem)
                 }
-                .padding(.top)
             }
-            ButtonlView()
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("Dinheirama 💸", displayMode: .inline)
+            .overlay(
+                VStack {
+                    Spacer()
+                    HStack {
+                        ButtonlView()
+                        Spacer()
+                        Text(String(format: "R$ %.2f", items.reduce(0, { $0 + $1.value })))
+                            .fontWeight(.bold)
+                            .frame(width: 140)
+                    }
+                    .padding()
+                    .background(Color.gray)
+                }
+            )
         }
+    }
+
+    private func deleteItem(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
     }
 }
 
